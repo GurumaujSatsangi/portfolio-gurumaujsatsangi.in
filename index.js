183864141e5100b8cdadc616f7a28ce2d844c50d
Gurumauj Satsangi <gurumaujsatsangi@gmail.com>
2026-06-09 13:35:43 +0530

    api for kodular

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.get("/send",async(req,res)=>{
+
+    return res.json({
+        success:true,
+        message:"Request Succesfully sent to Backend! Response received from Backend!"
+    })
+})
+
 app.get("/",async (req,res)=>{
     res.render("home.ejs");
 })
