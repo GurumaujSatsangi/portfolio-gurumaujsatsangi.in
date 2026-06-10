69004314e1cd223a059cc22935b324eea479f63b
Gurumauj Satsangi <gurumaujsatsangi@gmail.com>
2026-06-10 08:57:17 +0530

    add fileURLToPath import and define __filename and __dirname

--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 
 app.use(express.static("public"));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
