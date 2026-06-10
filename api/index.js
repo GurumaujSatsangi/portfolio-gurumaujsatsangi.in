494d46c512e875d0f10bef67b9bc8f58bdcaa593
Gurumauj Satsangi <gurumaujsatsangi@gmail.com>
2026-06-10 09:02:44 +0530

    update static file serving path and correct CGPA in home.ejs

--- api/index.js
+++ api/index.js
@@ -5,12 +5,13 @@ import { fileURLToPath } from 'url';
 
 const app = express();
 
-app.use(express.static("public"));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, '../public')));
 app.set('views', path.join(__dirname, '../views'));
 
 
